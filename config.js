3ea40d81217cfefb8385d738aec2bfaf9003ec87
antoinehage <me@antoinehage.com>
2020-04-21 14:46:03 -0400

    added the routes needed to manage item deployment

--- config.js
+++ config.js
@@ -92,6 +92,7 @@ module.exports = {
 				},
 				"commonFields": ["start", "limit", "keywords"]
 			},
+			
 			'/public/items': {
 				"_apiInfo": {
 					"l": "This API lists the public items matching certain keywords with option to select from what type and subtype.",
@@ -109,6 +110,7 @@ module.exports = {
 					"validation": {"type": "string"}
 				}
 			},
+			
 			'/items': {
 				"_apiInfo": {
 					"l": "This API lists the items matching certain keywords with option to select from what type and subtype.",
@@ -142,6 +144,14 @@ module.exports = {
 					"required": false,
 					"validation": {"type": "string"}
 				}
+			},
+			
+			"/item/configure/deploy": {
+				"_apiInfo": {
+					"l": "This API gets the configure deployment of an item including (allowed recipes, saved settings, and kubernetes settings)",
+					"group": "Item deploy"
+				},
+				"commonFields": ["id"]
 			}
 		},
 		"delete": {
@@ -166,23 +176,78 @@ module.exports = {
 						"pattern": /^[a-zA-Z0-9_-]+$/
 					}
 				}
+			},
+			
+			"/item/configure/deploy": {
+				"_apiInfo": {
+					"l": "This API deletes the configure deployment of an item",
+					"group": "Item deploy"
+				},
+				"commonFields": ["id"]
 			}
 		},
 		"put": {
-		
-			"/item/configure/deploy": {
+			
+			'/soajs/item/environments': {
 				"_apiInfo": {
-					"l": "This API updates the configure deployment of an item",
-					"group": "Item deploy"
+					"l": "This API updates the item environments from soajs catalog only",
+					"group": "SOAJS"
 				},
-				"commonFields": ["id"]
+				"commonFields": ["id"],
+				"type": {
+					"source": ['body.type'],
+					"required": true,
+					"validation": {
+						"type": "string",
+						"enum": ["blackList", "whitelist"]
+					}
+				},
+				"environments": {
+					"source": ['body.environments'],
+					"required": true,
+					"validation": {
+						"type": "array",
+						"minItems": 1
+					}
+				}
 			},
-			"/item/deploy": {
+			'/soajs/item/recipes': {
 				"_apiInfo": {
-					"l": "This API deploy an item",
-					"group": "Item deploy"
+					"l": "This API updates the item recipes from soajs catalog only",
+					"group": "SOAJS"
 				},
-				"commonFields": ["id"]
+				"commonFields": ["id"],
+				"recipes": {
+					"source": ['body.recipes'],
+					"required": true,
+					"validation": {
+						"type": "array",
+						"minItems": 1
+					}
+				}
+			},
+			'/soajs/item/acl': {
+				"_apiInfo": {
+					"l": "This API updates the item ACL from soajs catalog only",
+					"group": "SOAJS"
+				},
+				"commonFields": ["id"],
+				"type": {
+					"source": ['body.type'],
+					"required": true,
+					"validation": {
+						"type": "string",
+						"enum": ["blackList", "whitelist"]
+					}
+				},
+				"groups": {
+					"source": ['body.groups'],
+					"required": true,
+					"validation": {
+						"type": "array",
+						"minItems": 1
+					}
+				}
 			},
 			
 			'/item/environments': {
@@ -247,68 +312,6 @@ module.exports = {
 				}
 			},
 			
-			'/soajs/item/environments': {
-				"_apiInfo": {
-					"l": "This API updates the item environments from soajs catalog only",
-					"group": "SOAJS"
-				},
-				"commonFields": ["id"],
-				"type": {
-					"source": ['body.type'],
-					"required": true,
-					"validation": {
-						"type": "string",
-						"enum": ["blackList", "whitelist"]
-					}
-				},
-				"environments": {
-					"source": ['body.environments'],
-					"required": true,
-					"validation": {
-						"type": "array",
-						"minItems": 1
-					}
-				}
-			},
-			'/soajs/item/recipes': {
-				"_apiInfo": {
-					"l": "This API updates the item recipes from soajs catalog only",
-					"group": "SOAJS"
-				},
-				"commonFields": ["id"],
-				"recipes": {
-					"source": ['body.recipes'],
-					"required": true,
-					"validation": {
-						"type": "array",
-						"minItems": 1
-					}
-				}
-			},
-			'/soajs/item/acl': {
-				"_apiInfo": {
-					"l": "This API updates the item ACL from soajs catalog only",
-					"group": "SOAJS"
-				},
-				"commonFields": ["id"],
-				"type": {
-					"source": ['body.type'],
-					"required": true,
-					"validation": {
-						"type": "string",
-						"enum": ["blackList", "whitelist"]
-					}
-				},
-				"groups": {
-					"source": ['body.groups'],
-					"required": true,
-					"validation": {
-						"type": "array",
-						"minItems": 1
-					}
-				}
-			},
-			
 			"/item/resource": {
 				"_apiInfo": {
 					"l": "This API adds/updates an item of type resource to the catalog",
@@ -320,7 +323,6 @@ module.exports = {
 					"validation": item_resource_schema
 				}
 			},
-			
 			"/item/service": {
 				"_apiInfo": {
 					"l": "This API adds/updates an item of type service to the catalog",
@@ -332,7 +334,6 @@ module.exports = {
 					"validation": item_service_schema
 				}
 			},
-			
 			"/item/daemon": {
 				"_apiInfo": {
 					"l": "This API adds/updates an item of type daemon to the catalog",
@@ -344,7 +345,6 @@ module.exports = {
 					"validation": item_daemon_schema
 				}
 			},
-			
 			"/item/static": {
 				"_apiInfo": {
 					"l": "This API adds/updates an item of type static to the catalog",
@@ -356,7 +356,6 @@ module.exports = {
 					"validation": item_static_schema
 				}
 			},
-			
 			"/item/custom": {
 				"_apiInfo": {
 					"l": "This API adds/updates an item of type custom to the catalog",
@@ -368,7 +367,6 @@ module.exports = {
 					"validation": item_custom_schema
 				}
 			},
-			
 			"/item/config": {
 				"_apiInfo": {
 					"l": "This API adds/updates an item of type config to the catalog",
@@ -379,6 +377,42 @@ module.exports = {
 					"required": true,
 					"validation": item_config_schema
 				}
+			},
+			
+			"/item/redeploy": {
+				"_apiInfo": {
+					"l": "This API redeploy a deployed item",
+					"group": "Item deploy"
+				},
+				"commonFields": ["id"]
+			},
+			"/item/restart": {
+				"_apiInfo": {
+					"l": "This API restart a deployed item",
+					"group": "Item deploy"
+				},
+				"commonFields": ["id"]
+			},
+			"/item/cd": {
+				"_apiInfo": {
+					"l": "This API deploy an item used by CI",
+					"group": "Item deploy"
+				},
+				"commonFields": ["id"]
+			},
+			"/item/deploy": {
+				"_apiInfo": {
+					"l": "This API deploy an item",
+					"group": "Item deploy"
+				},
+				"commonFields": ["id"]
+			},
+			"/item/configure/deploy": {
+				"_apiInfo": {
+					"l": "This API updates the configure deployment of an item",
+					"group": "Item deploy"
+				},
+				"commonFields": ["id"]
 			}
 		}
 		
