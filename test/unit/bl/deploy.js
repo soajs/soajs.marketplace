890c3ce363a6ef42f31e4c2535807373ed0ce83b
Ragheb Bou Dargham <ragheb.abo.dargham@gmail.com>
2020-08-06 15:28:01 +0300

    update to lib/deploy.js test cases

--- test/unit/bl/deploy.js
+++ test/unit/bl/deploy.js
@@ -93,6 +93,78 @@ describe("Unit test for: bl/deploy.js - marketplace", function () {
 		});
 	});
 	
+	it("call saveConfigurationAndDeploy - fail no item", function (done) {
+		let BL = {
+			"marketplace": {
+				"handleError": (soajs, error) => {
+					return new Error(error);
+				},
+				"mp": {
+					"getModel": () => {
+						return {
+							getItem: (inputmaskData, cb) => {
+								return cb(null);
+							}
+						};
+					}
+				}
+			}
+		};
+		let bl = helper.requireModule('bl/deploy.js')(BL);
+		BL.localConfig = helper.requireModule("config.js");
+		let options = {};
+		bl.saveConfigurationAndDeploy(soajs, {}, options, (err) => {
+			assert.ok(err);
+			assert.deepEqual(err.message, "501");
+			done();
+		});
+	});
+	
+	it("call saveConfiguration - fail", function (done) {
+		let BL = {
+			"marketplace": {
+				"handleError": (soajs, error) => {
+					return new Error(error);
+				}
+			}
+		};
+		let bl = helper.requireModule('bl/deploy.js')(BL);
+		BL.localConfig = helper.requireModule("config.js");
+		let options = {};
+		bl.saveConfiguration(soajs, null, options, (err) => {
+			assert.ok(err);
+			assert.deepEqual(err.message, "400");
+			done();
+		});
+	});
+	
+	it("call saveConfiguration - fail mongo", function (done) {
+		let BL = {
+			"marketplace": {
+				"handleError": (soajs, error) => {
+					return new Error(error);
+				},
+				"mp": {
+					"getModel": () => {
+						return {
+							getItem: (inputmaskData, cb) => {
+								return cb(new Error("mongo error"));
+							}
+						};
+					}
+				}
+			}
+		};
+		let bl = helper.requireModule('bl/deploy.js')(BL);
+		BL.localConfig = helper.requireModule("config.js");
+		let options = {};
+		bl.saveConfiguration(soajs, {}, options, (err) => {
+			assert.ok(err);
+			assert.deepEqual(err.message, "602");
+			done();
+		});
+	});
+	
 	it("call saveConfigurationAndDeploy - success", function (done) {
 		sinon.stub(lib_deploy, 'deploy').callsFake(function fakeFn(soajs, inputmaskData, options, bl, cb) {
 			return cb(null, "true");
@@ -314,6 +386,102 @@ describe("Unit test for: bl/deploy.js - marketplace", function () {
 		});
 	});
 	
+	it("call saveConfiguration - fail error request", function (done) {
+		let BL = {
+			"marketplace": {
+				"handleError": (soajs, errCode) => {
+					return new Error(errCode);
+				},
+				"mp": {
+					"getModel": () => {
+						return {
+							getItem: (inputmaskData, cb) => {
+								return cb(null, {
+									"_id": "123"
+								});
+							},
+							update_item_configuration: (opts, cb) => {
+								return cb(null);
+							}
+						};
+					}
+				}
+			}
+		};
+		nock('http://192.168.5.200')
+			.get('/catalog/recipes/get')
+			.query({
+				id: "1234567890"
+			})
+			.reply(200, {
+				"result": false,
+				"errors": "request error"
+			});
+		let bl = helper.requireModule('bl/deploy.js')(BL);
+		BL.localConfig = helper.requireModule("config.js");
+		let options = {};
+		let inputmaskData = {
+			"_id": "123",
+			"name": "marketplace",
+			"type": "service",
+			"config": {
+				"env": "new",
+				"version": "1",
+				"cd": {
+					"strategy": "notify"
+				},
+				"settings": {
+					"memory": "10Mi",
+					"mode": "deployment",
+					"replicas": 1
+				},
+				"src": {
+					"from": {
+						"branch": "master",
+						"commit": "!@3"
+					},
+					"id": "12"
+				},
+				"recipe": {
+					"id": "1234567890",
+					"image": {
+						"name": "test",
+						"prefix": "soajs",
+						"tag": "1.1"
+					},
+					"readinessProbe": {},
+					"ports": {
+						"type": "test",
+						"portType": "Internal",
+						"values": [{
+							"name": "http",
+							"target": 80
+						}]
+					},
+					"env": {
+						"SECRET_ENV": {
+							"name": "secret name",
+							"key": "secret name"
+						},
+						"USER_ENV": "test data"
+					},
+					"sourceCode": {
+						"label": "test",
+						"catalog": "test",
+						"id": "test",
+						"version": "test",
+						"tag": "test",
+					}
+				}
+			}
+		};
+		bl.saveConfiguration(soajs, inputmaskData, options, (err) => {
+			assert.ok(err);
+			assert.deepEqual(err.message, "request error");
+			done();
+		});
+	});
+	
 	it("call saveConfiguration - success add image from recipe", function (done) {
 		let BL = {
 			"marketplace": {
@@ -325,7 +493,11 @@ describe("Unit test for: bl/deploy.js - marketplace", function () {
 						return {
 							getItem: (inputmaskData, cb) => {
 								return cb(null, {
-									"_id": "123"
+									"_id": "123",
+									"environments": {
+										"value": [],
+										"type": "blacklist"
+									}
 								});
 							},
 							update_item_configuration: (opts, cb) => {
@@ -421,6 +593,231 @@ describe("Unit test for: bl/deploy.js - marketplace", function () {
 		bl.saveConfiguration(soajs, inputmaskData, options, (err, response) => {
 			assert.ifError(err);
 			assert.deepEqual(response, "Catalog Item Configuration Successfully updated!");
+			done();
+		});
+	});
+	
+	it("call saveConfiguration - fail user input env not found", function (done) {
+		let BL = {
+			"marketplace": {
+				"handleError": (soajs, errCode) => {
+					return new Error(errCode);
+				},
+				"mp": {
+					"getModel": () => {
+						return {
+							getItem: (inputmaskData, cb) => {
+								return cb(null, {
+									"_id": "123",
+									"environments": {
+										"value": [],
+										"type": "blacklist"
+									}
+								});
+							},
+							update_item_configuration: (opts, cb) => {
+								return cb(null);
+							}
+						};
+					}
+				}
+			}
+		};
+		nock('http://192.168.5.200')
+			.get('/catalog/recipes/get')
+			.query({
+				id: "1234567890"
+			})
+			.reply(200, {
+				"result": true,
+				"data": {
+					"recipe": {
+						"buildOptions": {
+							"env": {
+								"SECRET_ENV": {
+									"type": "secret"
+								},
+								"USER_ENV": {
+									"type": "userInput"
+								}
+							}
+						},
+						"deployOptions": {
+							"image": {
+								"name": "test",
+								"prefix": "soajs",
+								"tag": "1.1"
+							},
+						}
+					}
+				}
+			});
+		let bl = helper.requireModule('bl/deploy.js')(BL);
+		BL.localConfig = helper.requireModule("config.js");
+		let options = {};
+		let inputmaskData = {
+			"_id": "123",
+			"name": "marketplace",
+			"type": "service",
+			"config": {
+				"env": "new",
+				"version": "1",
+				"cd": {
+					"strategy": "notify"
+				},
+				"settings": {
+					"memory": "10Mi",
+					"mode": "deployment",
+					"replicas": 1
+				},
+				"src": {
+					"from": {
+						"branch": "master",
+						"commit": "!@3"
+					},
+					"id": "12"
+				},
+				"recipe": {
+					"id": "1234567890",
+					"readinessProbe": {},
+					"ports": {
+						"type": "test",
+						"portType": "Internal",
+						"values": [{
+							"name": "http",
+							"target": 80
+						}]
+					},
+					"env": {
+						"SECRET_ENV": {
+							"name": "secret name",
+							"key": "secret name"
+						}
+					},
+					"sourceCode": {
+						"label": "test",
+						"catalog": "test",
+						"id": "test",
+						"version": "test",
+						"tag": "test",
+					}
+				}
+			}
+		};
+		bl.saveConfiguration(soajs, inputmaskData, options, (err) => {
+			assert.ok(err);
+			assert.deepEqual(err.message, "408");
+			done();
+		});
+	});
+	
+	it("call saveConfiguration - fail secret env not found", function (done) {
+		let BL = {
+			"marketplace": {
+				"handleError": (soajs, errCode) => {
+					return new Error(errCode);
+				},
+				"mp": {
+					"getModel": () => {
+						return {
+							getItem: (inputmaskData, cb) => {
+								return cb(null, {
+									"_id": "123",
+									"environments": {
+										"value": [],
+										"type": "blacklist"
+									}
+								});
+							},
+							update_item_configuration: (opts, cb) => {
+								return cb(null);
+							}
+						};
+					}
+				}
+			}
+		};
+		nock('http://192.168.5.200')
+			.get('/catalog/recipes/get')
+			.query({
+				id: "1234567890"
+			})
+			.reply(200, {
+				"result": true,
+				"data": {
+					"recipe": {
+						"buildOptions": {
+							"env": {
+								"SECRET_ENV": {
+									"type": "secret"
+								},
+								"USER_ENV": {
+									"type": "userInput"
+								}
+							}
+						},
+						"deployOptions": {
+							"image": {
+								"name": "test",
+								"prefix": "soajs",
+								"tag": "1.1"
+							},
+						}
+					}
+				}
+			});
+		let bl = helper.requireModule('bl/deploy.js')(BL);
+		BL.localConfig = helper.requireModule("config.js");
+		let options = {};
+		let inputmaskData = {
+			"_id": "123",
+			"name": "marketplace",
+			"type": "service",
+			"config": {
+				"env": "new",
+				"version": "1",
+				"cd": {
+					"strategy": "notify"
+				},
+				"settings": {
+					"memory": "10Mi",
+					"mode": "deployment",
+					"replicas": 1
+				},
+				"src": {
+					"from": {
+						"branch": "master",
+						"commit": "!@3"
+					},
+					"id": "12"
+				},
+				"recipe": {
+					"id": "1234567890",
+					"readinessProbe": {},
+					"ports": {
+						"type": "test",
+						"portType": "Internal",
+						"values": [{
+							"name": "http",
+							"target": 80
+						}]
+					},
+					"env": {
+						"USER_ENV": "test data"
+					},
+					"sourceCode": {
+						"label": "test",
+						"catalog": "test",
+						"id": "test",
+						"version": "test",
+						"tag": "test",
+					}
+				}
+			}
+		};
+		bl.saveConfiguration(soajs, inputmaskData, options, (err) => {
+			assert.ok(err);
+			assert.deepEqual(err.message, "409");
 			done();
 		});
 	});
@@ -768,6 +1165,7 @@ describe("Unit test for: bl/deploy.js - marketplace", function () {
 	});
 	
 	it("call saveConfiguration - fail update_item_configuration", function (done) {
+		soajs.urac.groups = [];
 		let BL = {
 			"marketplace": {
 				"handleError": (soajs, error) => {
@@ -778,7 +1176,13 @@ describe("Unit test for: bl/deploy.js - marketplace", function () {
 						return {
 							getItem: (inputmaskData, cb) => {
 								return cb(null, {
-									"_id": "123"
+									"_id": "123",
+									"settings" : {
+										"environments": {
+											"value": [],
+											"type": "whitelist"
+										}
+									}
 								});
 							},
 							update_item_configuration: (opts, cb) => {
@@ -872,6 +1276,117 @@ describe("Unit test for: bl/deploy.js - marketplace", function () {
 		bl.saveConfiguration(soajs, inputmaskData, options, (err) => {
 			assert.ok(err);
 			assert.deepEqual(err.message, "603");
+			done();
+		});
+	});
+	
+	it("call saveConfiguration - fail update_item_configuration mongo", function (done) {
+		delete soajs.urac;
+		let BL = {
+			"marketplace": {
+				"handleError": (soajs, error) => {
+					return new Error(error);
+				},
+				"mp": {
+					"getModel": () => {
+						return {
+							getItem: (inputmaskData, cb) => {
+								return cb(null, {
+									"_id": "123",
+									"settings" : {}
+								});
+							},
+							update_item_configuration: (opts, cb) => {
+								return cb(new Error('mongo error'));
+							}
+						};
+					}
+				}
+			}
+		};
+		nock('http://192.168.5.200')
+			.get('/catalog/recipes/get')
+			.query({
+				id: "1234567890"
+			})
+			.reply(200, {
+				"result": true,
+				"data": {
+					"recipe": {
+						"buildOptions": {
+							"env": {
+								"SECRET_ENV": {
+									"type": "secret"
+								},
+								"USER_ENV": {
+									"type": "userInput"
+								}
+							}
+						}
+					}
+				}
+			});
+		let bl = helper.requireModule('bl/deploy.js')(BL);
+		BL.localConfig = helper.requireModule("config.js");
+		let options = {};
+		let inputmaskData = {
+			"_id": "123",
+			"name": "marketplace",
+			"type": "service",
+			"config": {
+				"env": "new",
+				"version": "1",
+				"cd": {
+					"strategy": "notify"
+				},
+				"settings": {
+					"memory": "10Mi",
+					"mode": "deployment",
+					"replicas": 1
+				},
+				"src": {
+					"from": {
+						"branch": "master",
+						"commit": "!@3"
+					},
+					"id": "12"
+				},
+				"recipe": {
+					"id": "1234567890",
+					"image": {
+						"name": "test",
+						"prefix": "soajs",
+						"tag": "1.1"
+					},
+					"readinessProbe": {},
+					"ports": {
+						"type": "test",
+						"portType": "Internal",
+						"values": [{
+							"name": "http",
+							"target": 80
+						}]
+					},
+					"env": {
+						"SECRET_ENV": {
+							"name": "secret name",
+							"key": "secret name"
+						},
+						"USER_ENV": "test data"
+					},
+					"sourceCode": {
+						"label": "test",
+						"catalog": "test",
+						"id": "test",
+						"version": "test",
+						"tag": "test",
+					}
+				}
+			}
+		};
+		bl.saveConfiguration(soajs, inputmaskData, options, (err) => {
+			assert.ok(err);
+			assert.deepEqual(err.message, "602");
 			done();
 		});
 	});
